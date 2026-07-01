9ad54c68b84a6fc624badece2a5e03fbb6e15db0
VictorS357 <victorfransousa357@gmail.com>
2026-07-01 02:07:34 -0300

    create basic header

--- src/pages/Sucesso.jsx
+++ src/pages/Sucesso.jsx
@@ -3,25 +3,44 @@ import './Sucesso.css'
 
 function Sucesso() {
   return (
-    <main className="sucesso">
-      <div className="coracoes-flutuantes" aria-hidden="true">
-        {Array.from({ length: 16 }).map((_, i) => (
-          <span key={i} className="coracao" style={{ '--i': i }}>
-            💗
-          </span>
-        ))}
-      </div>
+    <>
+    <header>
+        <div className="h1-container">
+          <h1>
+            EU TE AMO MAIS QUE TUDO MEU AMOR
+          </h1>
+        </div>
 
-      <section className="cartao">
-        <h1>Ela disse sim! 🥰</h1>
-        <p className="subtitulo">
-          Agora somos oficialmente um casal. Te amo muito! ❤️
-        </p>
-        <Link to="/" className="btn-sim">
-          Voltar
-        </Link>
-      </section>
-    </main>
+        <div className="musica">
+          <iframe
+            data-testid="embed-iframe"
+            title="Player do Spotify"
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/track/3XrchC3CeuoPAprx9fqNKI?utm_source=generator&si=23e20edbff934823"
+            width="100%"
+            height="100"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          />
+        </div>
+      </header>
+
+      <main className="sucesso">
+        <div className="coracoes-flutuantes" aria-hidden="true">
+          {Array.from({ length: 16 }).map((_, i) => (
+            <span key={i} className="coracao" style={{ '--i': i }}>
+              💗
+            </span>
+          ))}
+        </div>
+
+        <section>
+          
+        </section>
+      </main>
+    </>
   )
 }
 
