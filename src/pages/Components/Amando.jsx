718df74be6ad8b5507d3f8153fb11bb885c10d3f
VictorS357 <victorfransousa357@gmail.com>
2026-07-01 03:05:01 -0300

    separate first date container

--- src/pages/Components/Amando.jsx
+++ src/pages/Components/Amando.jsx
@@ -2,13 +2,13 @@ import { Cronometro } from "./Cronometro";
 import './Amando.css';
 
 export function Amando() {
-    return (
-        <section className="tempo">
-          <div className="cronometro-container">
-            <h2 className="titulo">Eu te amo há:</h2>
-            <Cronometro />
-            <p>A partir de agr vamos só aumentar esses anos aí kkkkkk</p>
-          </div>
-        </section>
-    );
+  return (
+    <section className="tempo">
+      <div className="cronometro-container">
+        <h2 className="titulo">Eu te amo há:</h2>
+        <Cronometro />
+        <p>A partir de agr vamos só aumentar esses anos aí kkkkkk</p>
+      </div>
+    </section>
+  );
 }
--- src/pages/Sucesso.jsx
+++ src/pages/Sucesso.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 import { Header } from './Components/Header';
 import { Amando } from './Components/Amando';
 import { Cronometro } from './Components/Cronometro';
-import foto1 from '../assets/primeiro-encontro1.jpeg';
-import foto2 from '../assets/primeiro-encontro2.jpeg';
+
 import './Sucesso.css'
+import { PrimeiroEncontro } from './Components/PrimeiroEncontro';
 
 
 
@@ -24,16 +24,7 @@ function Sucesso() {
 
         <Amando />
 
-        <section className="primeiro-encontro">
-          <div className="encontro-container">
-            <h2 className="titulo">Nosso primeiro encontro</h2>
-            <div className="galeria">
-              <img className="foto" src={foto1} alt="Nosso primeiro encontro" />
-              <img className="foto" src={foto2} alt="Nosso primeiro encontro" />
-            </div>
-            <p className="texto">Nossa amor, parando pra pensar agora, se eu voltasse no tempo exatamente para esse dia, nunca na vida que eu iria imaginar que viveríamos tudo isso que vivemos até agora. Na época eu tinha muitas perguntas e preocupações... "e se não der certo?", "e se ela não gostar de mim?", "será que eu posso ser eu mesmo e expressar td o que eu sinto?". E mal sabia eu que desde então, eu me apaixonaria cada vez mais por ti, a cada dia que passa. E lógico que eu sonhava muito com o dia do pedido de namoro, o dia em que eu finalmente poderia te chamar de <span>minha namorada</span>. Hoje eu posso olhar pra trás e dizer que eu viveria tudo novamente, exatamente da forma que tudo aconteceu. </p>
-          </div>
-        </section>
+        <PrimeiroEncontro />
       </main>
     </>
   )
